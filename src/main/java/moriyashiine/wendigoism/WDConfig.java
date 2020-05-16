250fbd9571578a121d63f687939e50733d180f39
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2020-05-16 12:47:06 -0500

    Update WDConfig.java

--- src/main/java/moriyashiine/wendigoism/WDConfig.java
+++ src/main/java/moriyashiine/wendigoism/WDConfig.java
@@ -6,7 +6,6 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.Arrays;
 import java.util.List;
 
-/** File created by mason on 4/18/20 **/
 public class WDConfig {
 	public static final WDConfig INSTANCE;
 	static final ForgeConfigSpec SPEC;

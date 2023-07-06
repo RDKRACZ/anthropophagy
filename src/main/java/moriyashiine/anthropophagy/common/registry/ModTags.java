1838f6ea2e8db7799fdce9bd45726707b61b08e9
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-07-06 00:52:50 -0500

    fix #9

--- src/main/java/moriyashiine/anthropophagy/common/registry/ModTags.java
+++ src/main/java/moriyashiine/anthropophagy/common/registry/ModTags.java
@@ -6,14 +6,14 @@ package moriyashiine.anthropophagy.common.registry;
 
 import moriyashiine.anthropophagy.common.Anthropophagy;
 import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class ModTags {
 	public static class Items {
-		public static final TagKey<Item> KNIVES = TagKey.of(Registries.ITEM.getKey(), new Identifier("fabric", "tools/knives"));
+		public static final TagKey<Item> KNIVES = TagKey.of(RegistryKeys.ITEM, new Identifier("c", "knives"));
 
-		public static final TagKey<Item> FLESH = TagKey.of(Registries.ITEM.getKey(), Anthropophagy.id("flesh"));
+		public static final TagKey<Item> FLESH = TagKey.of(RegistryKeys.ITEM, Anthropophagy.id("flesh"));
 	}
 }

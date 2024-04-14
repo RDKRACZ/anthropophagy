1946cc6dbfa4b4349d3f1e72532743c6a81966b8
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-04-14 17:40:27 -0500

    cannibal night vision is now gradual

--- src/main/java/moriyashiine/anthropophagy/mixin/client/LightmapTextureManagerMixin.java
+++ src/main/java/moriyashiine/anthropophagy/mixin/client/LightmapTextureManagerMixin.java
@@ -4,11 +4,13 @@
 
 package moriyashiine.anthropophagy.mixin.client;
 
+import moriyashiine.anthropophagy.common.component.entity.CannibalLevelComponent;
 import moriyashiine.anthropophagy.common.init.ModEntityComponents;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,8 +26,9 @@ public class LightmapTextureManagerMixin {
 
 	@ModifyVariable(method = "update", at = @At("STORE"), ordinal = 6)
 	private float anthropophagy$cannibalNightVision(float value) {
-		if (client.player != null && ModEntityComponents.CANNIBAL_LEVEL.get(client.player).getCannibalLevel() >= 50) {
-			return Math.max(1, value);
+		if (client.player != null) {
+			CannibalLevelComponent cannibalLevelComponent = ModEntityComponents.CANNIBAL_LEVEL.get(client.player);
+			return MathHelper.clamp(MathHelper.lerp((cannibalLevelComponent.getCannibalLevel() - 30) / 20F, 0, 1F), value, 1);
 		}
 		return value;
 	}
